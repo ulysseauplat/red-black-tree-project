7c6b629c5ddf41cc795d7bd6fefa9553172d7b6b
uauplat <ulysse.auplat@etudiant.univ-lr.fr>
2025-09-05 01:29:17 +0200

    features: redblacktree and node skeleton

--- src/main/java/com/redblacktree/Node.java
+++ src/main/java/com/redblacktree/Node.java
@@ -1,10 +1,53 @@
 package com.redblacktree;
 
 /**
- * Hello world!
+ * The node classe representing a node with his data, color, left child, right child and parent
  */
-public class Node {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
+public class Node 
+ {
+        private int data;
+        private boolean color;
+        private Node left, right, parent;
+        public static final boolean RED = true;
+        public static final boolean BLACK = false;
+
+
+        public Node(int data) {                 // the automatic color of a new node is red
+            this.data = data;
+            this.color = RED;
+            left = right = parent = null;
+        }
+
+        public int getData() {
+            return data;
+        }     
+        public boolean getColor() {
+            return color;
+        }
+        public Node getLeft() {
+            return left;
+        }
+        public Node getRight() {
+            return right;
+        }
+        public Node getParent() {
+            return parent;
+        }
+        public void setData(int data) {
+            this.data = data;
+        }
+        public void setColor(boolean color) {
+            this.color = color;
+        }
+        public void setLeft(Node left) {
+            this.left = left;
+        }
+        public void setRight(Node right) {
+            this.right = right;
+        }
+        public void setParent(Node parent) {
+            this.parent = parent;
+        }
+
+
 }
