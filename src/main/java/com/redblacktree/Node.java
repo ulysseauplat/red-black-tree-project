9e03397df45bf8eb292501b885853f73d192f9fe
uauplat <ulysse.auplat@etudiant.univ-lr.fr>
2025-09-05 01:01:51 +0200

    feature: maven setup

---
+++ src/main/java/com/redblacktree/Node.java
@@ -0,0 +1,10 @@
+package com.redblacktree;
+
+/**
+ * Hello world!
+ */
+public class Node {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ src/main/java/com/redblacktree/RedBlackTree.java
@@ -0,0 +1,10 @@
+package com.redblacktree;
+
+/**
+ * Hello world!
+ */
+public class RedBlackTree {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
